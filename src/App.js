1d2b3e3f60bb8475cbd8cbef738ec139c7321c50
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-10 03:01:03 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Banner from './components/Banner/Banner';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Banner></Banner>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+
+      </Routes>
     </div>
   );
 }
